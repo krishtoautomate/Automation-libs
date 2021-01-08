f2306f1d6483c27000fdb5c4437f40d4d729a5c5
Krish Pavulur <>
2021-01-08 17:13:55 -0500

    jira cleanup

--- src/test/java/com/Listeners/TestListener.java
+++ src/test/java/com/Listeners/TestListener.java
@@ -31,25 +31,24 @@ import com.base.Jira;
 import com.base.ScreenShotManager;
 import com.base.TestBase;
 
-public class TestListener extends TestListenerAdapter implements ISuiteListener, ITestListener, IInvokedMethodListener  {
-	
+public class TestListener extends TestListenerAdapter implements ISuiteListener, ITestListener, IInvokedMethodListener {
+
 	protected ReportBuilder reporter = new ReportBuilder();
-	
+
 	Jira jiraReporter = new Jira();
-	
+
 	@Override
 	public void onTestStart(ITestResult testResult) {
-		
+
 		Date date = new Date();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
 		sdf.setTimeZone(TimeZone.getTimeZone("EST"));
 		String dateANDtime = sdf.format(date.getTime());
-		
+
 		String start = dateANDtime;
-		
+
 		testResult.setAttribute("start", start);
-		
-		
+
 	}
 
 	@Override
@@ -65,48 +64,47 @@ public class TestListener extends TestListenerAdapter implements ISuiteListener,
 		String buildNo = System.getenv("BUILD_NUMBER");
 		String environment = System.getenv("ENVIRONMENT");
 		String testName = testResult.getMethod().getMethodName();
-		
+
 		Object testClass = testResult.getInstance();
 		ExtentTest test = ((TestBase) testClass).getExtentTest();
-		
-		test.log(Status.INFO, testName+ " - Completed as Success");
-		
-	    //Categories
-        test.assignCategory(platForm);
-        test.assignCategory(deviceName);
-        test.assignCategory("Passed");
-        
-        //DB update
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+
+		test.log(Status.INFO, testName + " - Completed as Success");
+
+		// Categories
+		test.assignCategory(platForm);
+		test.assignCategory(deviceName);
+		test.assignCategory("Passed");
+
+		// DB update
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 		LocalDateTime now = LocalDateTime.now();
 		String date_time = dtf.format(now);
-        
+
 		reporter.report(date_time, "LM", buildNo, environment, testName, deviceName, platForm, "PASS", "");
-		
-		
-		//Jira
+
+		// Jira
 		Date date = new Date();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
 		sdf.setTimeZone(TimeZone.getTimeZone("EST"));
 		String dateANDtime = sdf.format(date.getTime());
-		
+
 		String testKey = null;
 		try {
 			testKey = testResult.getAttribute("testKey").toString();
 		} catch (Exception e) {
-			//ignore
+			// ignore
 		}
-		
-		if(testKey!=null) {
-			String start  = testResult.getAttribute("start").toString();
+
+		if (testKey != null) {
+			String start = testResult.getAttribute("start").toString();
 			String finish = dateANDtime;
 //			jiraReporter.setTestInfo("testKey", testKey);
 //			jiraReporter.setTestInfo("start", start);
 //			jiraReporter.setTestInfo("finish", finish);
 //			jiraReporter.setTestInfo("status", "PASS");
 //			jiraReporter.setTestInfo("comment", testName+"("+platForm+")");
-			
-			jiraReporter.addTest(testKey, start, finish, "PASS", testName+"("+platForm+")");
+
+			jiraReporter.addTest(testKey, start, finish, "PASS", testName + "(" + platForm + ")");
 		}
 	}
 
@@ -114,7 +112,7 @@ public class TestListener extends TestListenerAdapter implements ISuiteListener,
 	public synchronized void onTestFailure(ITestResult testResult) {
 		/*
 		 * get device details
-		*/
+		 */
 		Map<String, String> testParams = testResult.getTestContext().getCurrentXmlTest().getAllParameters();
 		String udid = testParams.get("udid");
 		DeviceDAO deviceinfoProvider = new DeviceDAO(udid);
@@ -123,59 +121,60 @@ public class TestListener extends TestListenerAdapter implements ISuiteListener,
 		String buildNo = System.getenv("BUILD_NUMBER");
 		String environment = System.getenv("ENVIRONMENT");
 		String testName = testResult.getMethod().getMethodName();
-		
+
 		Object testClass = testResult.getInstance();
 		WebDriver driver = ((TestBase) testClass).getDriver();
 		Logger log = ((TestBase) testClass).getLog();
 		ExtentTest test = ((TestBase) testClass).getExtentTest();
-		
-		if(driver != null) {
-			log.error("Test failed : "+ testName + " : "+ udid+"_"+deviceName);
+
+		if (driver != null) {
+			log.error("Test failed : " + testName + " : " + udid + "_" + deviceName);
 			try {
 				ScreenShotManager screenShotManager = new ScreenShotManager(driver);
 				String ScreenShot = screenShotManager.getScreenshot();
-				
-				test.fail("Failed Test case : " + testName + "\n" + testResult.getThrowable(), 
+
+				test.fail("Failed Test case : " + testName + "\n" + testResult.getThrowable(),
 						MediaEntityBuilder.createScreenCaptureFromPath(ScreenShot).build());
-				
+
 				String errorXML = driver.getPageSource();
 				test.info(MarkupHelper.createCodeBlock(errorXML));
-				
+
 			} catch (WebDriverException e) {
 				test.fail("Failed Test case : " + testName + "\n" + testResult.getThrowable());
 			}
-			
-		    //Categories
-	        test.assignCategory(platForm);
-	        test.assignCategory(deviceName);
+
+			// Categories
+			test.assignCategory(platForm);
+			test.assignCategory(deviceName);
 			test.assignCategory("Failed");
-			
-			//DB
-			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+
+			// DB
+			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 			LocalDateTime now = LocalDateTime.now();
 			String date_time = dtf.format(now);
-				
-			//Emailable Test Summary
-			reporter.report(date_time, "LM", buildNo, environment, testName, deviceName, platForm, "FAIL", testResult.getThrowable().toString());
-			
+
+			// Emailable Test Summary
+			reporter.report(date_time, "LM", buildNo, environment, testName, deviceName, platForm, "FAIL",
+					testResult.getThrowable().toString());
+
 		}
-		//Jira
+		// Jira
 		Date date = new Date();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
 		sdf.setTimeZone(TimeZone.getTimeZone("EST"));
 		String dateANDtime = sdf.format(date.getTime());
-		
+
 		String testKey = null;
 		try {
 			testKey = testResult.getAttribute("testKey").toString();
 		} catch (Exception e) {
-			//ignore
+			// ignore
 		}
-		
-		if(testKey!=null) {
-			String start  = testResult.getAttribute("start").toString();
+
+		if (testKey != null) {
+			String start = testResult.getAttribute("start").toString();
 			String finish = dateANDtime;
-			jiraReporter.addTest(testKey, start, finish, "FAIL", testName+"("+platForm+")");
+			jiraReporter.addTest(testKey, start, finish, "FAIL", testName + "(" + platForm + ")");
 		}
 	}
 
@@ -188,14 +187,14 @@ public class TestListener extends TestListenerAdapter implements ISuiteListener,
 		String udid = testParams.get("udid");
 		DeviceDAO deviceinfoProvider = new DeviceDAO(udid);
 		String deviceName = deviceinfoProvider.getDeviceName();
-		
+
 		Object testClass = testResult.getInstance();
 		Logger log = ((TestBase) testClass).getLog();
-		log.warn("Test Skipped : "+ testResult.getMethod().getMethodName() + " : "+ udid+"_"+deviceName);
-		
+		log.warn("Test Skipped : " + testResult.getMethod().getMethodName() + " : " + udid + "_" + deviceName);
+
 		ExtentTest test = ((TestBase) testClass).getExtentTest();
 		ExtentReports extent = ((TestBase) testClass).getExtentReports();
-		
+
 		try {
 			extent.removeTest(test);
 		} catch (Exception e) {
@@ -205,64 +204,62 @@ public class TestListener extends TestListenerAdapter implements ISuiteListener,
 
 	@Override
 	public void onStart(ISuite suite) {
-		reporter.initialize();//Emailable Report
-		
+		reporter.initialize();// Emailable Report
+
 		Date date = new Date();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
 		sdf.setTimeZone(TimeZone.getTimeZone("EST"));
 		String dateANDtime = sdf.format(date.getTime());
-		
-		//Jira report
+
+		// Jira report
 		String buildNo = System.getenv("BUILD_NUMBER");
 		String jobName = System.getenv("JOB_NAME");
-		String buildUrl =  System.getenv("BUILD_URL");
+		String buildUrl = System.getenv("BUILD_URL");
 		String testPlanKey = System.getenv("TEST_PLAN_KEY");
-		String excutionSummary = jobName+buildNo;
+		String excutionSummary = jobName + buildNo;
 		String excutionDescription = buildUrl;
 		String startDate = dateANDtime;
 		jiraReporter.setTestExecutionInfo("summary", excutionSummary);
-		jiraReporter.setTestExecutionInfo("testPlanKey", testPlanKey);//"MAEAUTO-311"
+		jiraReporter.setTestExecutionInfo("testPlanKey", testPlanKey);// "MAEAUTO-311"
 		jiraReporter.setTestExecutionInfo("description", excutionDescription);
 		jiraReporter.setTestExecutionInfo("startDate", startDate);
-		
+
 	}
 
 	@Override
 	public void onFinish(ISuite suite) {
 		String emailReport = Constants.EMAIL_REPORT;
 		File file = new File(emailReport);
-        if(file.delete()) {
-        	//delete if exists
-        }
-        String buildNo = System.getenv("BUILD_NUMBER");
-        if(buildNo != null)
+		if (file.delete()) {
+			// delete if exists
+		}
+		String buildNo = System.getenv("BUILD_NUMBER");
+		if (buildNo != null)
 			reporter.writeResults(emailReport);
-        
-        //Jira report
-        Date date = new Date();
+
+		// Jira report
+		Date date = new Date();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
 		sdf.setTimeZone(TimeZone.getTimeZone("EST"));
 		String dateANDtime = sdf.format(date.getTime());
 		String finishDate = dateANDtime;
 		jiraReporter.setTestExecutionInfo("finishDate", finishDate);
-        jiraReporter.addInfo();
-        jiraReporter.addTests();
-        jiraReporter.CreatejiraReport(Constants.JIRA_REPORT);
-        
+		jiraReporter.addInfo();
+		jiraReporter.addTests();
+		jiraReporter.CreatejiraReport(Constants.JIRA_REPORT);
+
 	}
 
 	@Override
 	public void beforeInvocation(IInvokedMethod method, ITestResult testResult) {
 		// TODO Auto-generated method stub
-	
+
 	}
 
 	@Override
 	public void afterInvocation(IInvokedMethod method, ITestResult testResult) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-		
-	
 }
