bf266271ceae34609a365f3d264f7ab29fcc65d3
Krish Pavulur <>
2021-03-09 10:35:28 -0500

    index upadte

--- src/test/java/Test/TestData.java
+++ src/test/java/Test/TestData.java
@@ -1,23 +1,21 @@
 package Test;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import org.json.simple.parser.ParseException;
-
-import com.DataManager.TestDataManager;
-
 public class TestData {
 
-	public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
+	public static void main(String[] args)  {
 		// TODO Auto-generated method stub
 		
 //		TestDataManager testDataManager = new TestDataManager("src/test/resources/MBM_Database.txt", "MBMobility_TestCases.Login", "ios");
 		
-		TestDataManager testDataManager = new TestDataManager("src/test/resources/MBM_Database.txt", "MBMobility_TestCases.InValidLogin", "ios");
+//		TestDataManager testDataManager = new TestDataManager("src/test/resources/MBM_Database.txt", "MBMobility_TestCases.InValidLogin", "ios");
 		
 //		System.out.println(testDataManager.getValue("name"));
-		System.out.println(testDataManager.getJsonValue(0, "name"));
+//		System.out.println(testDataManager.getJsonValue(0, "name"));
+		
+		String x = "2021";
+//		x.substring(x.length()-2);
+		
+		System.out.println(x.substring(x.length()-2));
 		
 	}
 
