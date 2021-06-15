59c4783afb4e1af89b4930aa94659a321fcb4f69
Tushar Sharma <tushar.sharma@bell.ca>
2021-06-15 13:49:54 -0400

    add slack reporter

---
+++ src/test/java/com/ReportManager/SlackReporter.java
@@ -0,0 +1,63 @@
+package com.ReportManager;
+
+import java.io.File;
+import java.util.Arrays;
+import com.slack.api.Slack;
+import com.slack.api.SlackConfig;
+import com.slack.api.methods.MethodsClient;
+import com.slack.api.methods.request.chat.ChatPostMessageRequest;
+import com.slack.api.methods.request.files.FilesUploadRequest;
+import com.slack.api.methods.response.chat.ChatPostMessageResponse;
+
+public class SlackReporter {
+
+
+  public void send_Message_To_Channel(String message, String channel) {
+    try {
+      String authToken = System.getenv("SLACK_AUTH");
+      Slack slack = Slack.getInstance(get_Config());
+
+      MethodsClient methods = slack.methods(authToken);
+
+      ChatPostMessageRequest request =
+          ChatPostMessageRequest.builder().channel(channel).text(message).build();
+
+      methods.chatPostMessage(request);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+  }
+
+  public void send_Failure_Data_To_Channel(String message, String ssPath, String channel) {
+    try {
+      String authToken = System.getenv("SLACK_AUTH");
+      Slack slack = Slack.getInstance(get_Config());
+      MethodsClient methods = slack.methods(authToken);
+
+      ChatPostMessageRequest request =
+          ChatPostMessageRequest.builder().channel(channel).text(message).build();
+
+      ChatPostMessageResponse response = methods.chatPostMessage(request);
+
+      if (ssPath != null) {
+        FilesUploadRequest filerequest =
+            FilesUploadRequest.builder().channels(Arrays.asList(channel)).file(new File(ssPath))
+                .filename("FailureSS").threadTs(response.getTs()).build();
+
+        methods.filesUpload(filerequest);
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+  }
+
+  private SlackConfig get_Config() {
+    SlackConfig config = new SlackConfig();
+    config.setProxyUrl("http://fastweb.int.bell.ca:8083");
+    return config;
+  }
+
+}
