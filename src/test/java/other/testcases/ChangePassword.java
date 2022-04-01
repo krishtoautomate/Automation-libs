52e08b8fb650bbbd55bc078bb8d5ba8f2de64b02
krish.pavulur <krish.pavulur@bell.ca>
2022-04-01 16:38:25 -0400

    deviceinfo update

--- src/test/java/other/testcases/ChangePassword.java
+++ src/test/java/other/testcases/ChangePassword.java
@@ -9,7 +9,6 @@ import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Scanner;
-import org.json.JSONException;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
@@ -19,7 +18,7 @@ public class ChangePassword {
   public static void main(String[] args) throws IOException, URISyntaxException,
       NoSuchAlgorithmException, KeyManagementException, InterruptedException {
 
-    Scanner s = new Scanner(new File("/Users/krish/Downloads/automationMBM.csv"));
+    Scanner s = new Scanner(new File("/Users/krish/Downloads/MLM_Auto_March.csv"));
     ArrayList<String> list = new ArrayList<String>();
     while (s.hasNext()) {
       list.add(s.next());
@@ -32,7 +31,7 @@ public class ChangePassword {
       String[] parts = bup.split(",");
       // String mdn = parts[0].trim();
       String user = parts[0].trim();
-      String password = parts[1].trim();
+      String password = "Fibe1234$";// "Lucky1234$";// parts[1].trim();
 
       System.out.println("user : " + user);
       System.out.println("password : " + password);
@@ -43,29 +42,30 @@ public class ChangePassword {
 
       // Specify the base URL to the RESTful web service
       // RestAssured.baseURI = Constants.BASE_URI;
-      RestAssured.baseURI =
-          // "https://api.virginplus.ca/channelvirginext";
-          "https://apigate.bell.ca/channelbellcaext";
+      RestAssured.baseURI = "https://api.luckymobile.ca/channelluckyext";
+      // "https://api.virginplus.ca/channelvirginext";
+      // "https://apigate.bell.ca/channelbellcaext";
+
+      String Brand = "L";// "B" "V";
+      String applicationid = "MLM_IOS";// MVM_IOS
 
       try {
 
         Thread.sleep(5000);
-        Response response = given().auth().basic(user, password)
-            // .header("Host", Constants.API_HOST)
-            .header("Connection", "keep-alive").header("Accept", "*/*")
-            .header("Accept-Language", "en-ca").header("Cache-Control", "no-cache")
-            .header("Accept-Encoding", "gzip, deflate, br").header("applicationid", "MBM_IOS")
+        Response response = given().relaxedHTTPSValidation().auth().basic(user, password)
+            .header("accept-language", "en-ca").header("brand", Brand)
+            .header("applicationid", applicationid)
             .get(RestAssured.baseURI + "/Authentication/BUP");
 
+        System.out.println(response.getBody().path("status").toString()); // .asPrettyString().);
 
-        System.out.println(response.getBody().asPrettyString());
         System.out.println("----------");
 
         // String userId = response.path("userId").toString();
-
         // Thread.sleep(5000);
         //
-        // given().header("accept-language", "EN-CA").header("province", "ON").header("brand", "B")
+        // given().header("accept-language", "EN-CA").header("province", "ON").header("brand",
+        // Brand)
         // .header("Accept", "*/*").header("Cache-Control", "no-cache")
         // .header("Host", "api.virginmobile.ca").header("Accept-Encoding", "gzip, deflate")
         // .header("Connection", "keep-alive").cookies(response.cookies()).when()
@@ -76,15 +76,16 @@ public class ChangePassword {
 
         Thread.sleep(2000);
 
-        String newPassword = "Crave1234$";
-        given().header("Accept-Language", "EN-CA").header("brand", "B")
-            .header("Content-Type", "application/json").header("applicationid", "MBM_IOS")
-            .body("{\r\n\"CurrentKey\" : \"" + password + "\",\r\n\"NewKey\" : \"" + newPassword
-                + "\",\r\n\"Username\" : \"" + user + "\"\r\n}")
-            .cookies(response.cookies()).when()
-            .put(RestAssured.baseURI + "/UXP.Services/ecare/Profile/UserProfile/Me/Password");
+        // String newPassword = "Fibe1234$";
+        // given().header("Accept-Language", "EN-CA").header("brand", Brand)
+        // .header("Content-Type", "application/json").header("applicationid", applicationid)
+        // .body("{\r\n'CurrentKey' : '" + password + "',\r\n'NewKey' : '" + newPassword
+        // + "',\r\n'Username' : '" + user + "'\r\n}")
+        // .cookies(response.cookies()).when()
+        // .put(RestAssured.baseURI + "/UXP.Services/ecare/Profile/UserProfile/Me/Password");
 
-      } catch (JSONException | InterruptedException | NullPointerException e) {
+
+      } catch (Exception e) {
         // ignore
         System.out.println("Failed: " + user + ":" + password);
 
