3d291b7576ebdff833ff576a7f962e8a39cd138d
krish.pavulur <krish.pavulur@bell.ca>
2022-10-10 16:07:37 -0400

    param capabilities added

--- src/test/java/com/base/CapabilitiesManager.java
+++ src/test/java/com/base/CapabilitiesManager.java
@@ -4,15 +4,8 @@ package com.base;
  **/
 
 import com.DataManager.DeviceInfoReader;
-import com.DataManager.TestDataManager;
 import com.Utilities.Constants;
-import com.google.gson.JsonArray;
 import io.appium.java_client.remote.MobileCapabilityType;
-import java.io.FileReader;
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.util.Map;
-
 import org.apache.log4j.Logger;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -22,108 +15,110 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.ITestResult;
 import org.testng.Reporter;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+
 /**
  * Created by Krish on 21.01.2019.
  */
 public class CapabilitiesManager {
 
-  private static Logger log = Logger.getLogger(CapabilitiesManager.class.getName());
+    static int devicePort = 8100;
+    private static Logger log = Logger.getLogger(CapabilitiesManager.class.getName());
 
-  static int devicePort = 8100;
+    @SuppressWarnings("unchecked")
+    public synchronized DesiredCapabilities setCapabilities() {
 
-  @SuppressWarnings("unchecked")
-  public synchronized DesiredCapabilities setCapabilities() {
+        devicePort++;
 
-    devicePort++;
+        DesiredCapabilities capabilities = new DesiredCapabilities();
 
-    DesiredCapabilities capabilities = new DesiredCapabilities();
+        ITestResult iTestResult = Reporter.getCurrentTestResult();
+        Map<String, String> testParams =
+                iTestResult.getTestContext().getCurrentXmlTest().getAllParameters();
 
-    ITestResult iTestResult = Reporter.getCurrentTestResult();
-    Map<String, String> testParams =
-        iTestResult.getTestContext().getCurrentXmlTest().getAllParameters();
+        String platForm = testParams.get("platForm");
+        String udid = testParams.get("udid");
+        String pCapabilities = testParams.get("capabilities");
 
-    String platForm = testParams.get("platForm");
-    String udid = testParams.get("udid");
-    String pCapabilities = testParams.get("capabilities");
+        String deviceName = "Android".equalsIgnoreCase(platForm) ? "Android Device" : "iPhone";
 
-    String deviceName = "Android".equalsIgnoreCase(platForm) ? "Android Device":"iPhone";
-
-    if(udid != null) {
-      capabilities.setCapability(MobileCapabilityType.UDID, udid);
-
-
-      DeviceInfoReader deviceInfoReader = new DeviceInfoReader(udid);
-      deviceName = deviceInfoReader.getString("name");
-      devicePort = deviceInfoReader.getInt("devicePort");
-      if ("Android".equalsIgnoreCase(platForm)) {
-        capabilities.setCapability("systemPort", devicePort);
-      }
-      if ("iOS".equalsIgnoreCase(platForm)) {
-        capabilities.setCapability("wdaLocalPort", devicePort);
-      }
-    }
-    else{
-
-      if ("Android".equalsIgnoreCase(platForm)) {
-        capabilities.setCapability("systemPort", devicePort);
-      }
-      if ("iOS".equalsIgnoreCase(platForm)) {
-        capabilities.setCapability("wdaLocalPort", devicePort);
-      }
-
-    }
-    capabilities.setCapability("deviceName", deviceName);
-
-    String capabilitiesName = "Android".equalsIgnoreCase(platForm) ? "ANDROID" : "IOS";
-
-    try {
-      JSONObject jsonObject =
-          (JSONObject) new JSONParser().parse(new FileReader(Constants.CAPABILITIES));
-      JSONArray jsonArray = (JSONArray) jsonObject.get(capabilitiesName);
-      JSONObject jObj = (JSONObject) jsonArray.get(0);
-
-      jObj.keySet().forEach(key -> {
-        capabilities.setCapability(key.toString(), jObj.get(key));
-      });
-
-    } catch (IOException | ParseException | NullPointerException ex) {
-      log.error(("failed to set global capabilities"));
-    }
-
-    try {
-      String pTestData = testParams.get("p_Testdata");
-      JSONObject jsonObject = (JSONObject) new JSONParser().parse(new FileReader(pTestData));
-      String className = iTestResult.getInstanceName();
-      JSONArray jsonArray = (JSONArray) jsonObject.get(className);
-      JSONObject jObj = (JSONObject) jsonArray.get(0);
-      JSONObject jAObject = (JSONObject) jObj.get("capabilities");
-
-      for (Object keyStr : jAObject.keySet()) {
-        capabilities.setCapability(keyStr.toString(), jAObject.get(keyStr).toString());
-      }
-    } catch (Exception e) {
-      log.error(("No test capabilities found!"));
-    }
+        if (udid != null) {
+            capabilities.setCapability(MobileCapabilityType.UDID, udid);
 
 
-    if(pCapabilities != null) {
+            DeviceInfoReader deviceInfoReader = new DeviceInfoReader(udid);
+            deviceName = deviceInfoReader.getString("name");
+            devicePort = deviceInfoReader.getInt("devicePort");
+            if ("Android".equalsIgnoreCase(platForm)) {
+                capabilities.setCapability("systemPort", devicePort);
+            }
+            if ("iOS".equalsIgnoreCase(platForm)) {
+                capabilities.setCapability("wdaLocalPort", devicePort);
+            }
+        } else {
 
-      pCapabilities = pCapabilities.replaceAll("'","\"");
+            if ("Android".equalsIgnoreCase(platForm)) {
+                capabilities.setCapability("systemPort", devicePort);
+            }
+            if ("iOS".equalsIgnoreCase(platForm)) {
+                capabilities.setCapability("wdaLocalPort", devicePort);
+            }
 
-      try {
-        JSONObject jsonObject = (JSONObject) new JSONParser().parse(pCapabilities);
-
-        for (Object keyStr : jsonObject.keySet()) {
-          capabilities.setCapability(keyStr.toString(), jsonObject.get(keyStr).toString());
         }
-      } catch (ParseException e) {
-        log.error(("No test parameter capabilities found!"));
-      }
+        capabilities.setCapability("deviceName", deviceName);
+
+        String capabilitiesName = "Android".equalsIgnoreCase(platForm) ? "ANDROID" : "IOS";
+
+        try {
+            JSONObject jsonObject =
+                    (JSONObject) new JSONParser().parse(new FileReader(Constants.CAPABILITIES));
+            JSONArray jsonArray = (JSONArray) jsonObject.get(capabilitiesName);
+            JSONObject jObj = (JSONObject) jsonArray.get(0);
+
+            jObj.keySet().forEach(key -> {
+                capabilities.setCapability(key.toString(), jObj.get(key));
+            });
+
+        } catch (IOException | ParseException | NullPointerException ex) {
+            log.error(("failed to set global capabilities"));
+        }
+
+        try {
+            String pTestData = testParams.get("p_Testdata");
+            JSONObject jsonObject = (JSONObject) new JSONParser().parse(new FileReader(pTestData));
+            String className = iTestResult.getInstanceName();
+            JSONArray jsonArray = (JSONArray) jsonObject.get(className);
+            JSONObject jObj = (JSONObject) jsonArray.get(0);
+            JSONObject jAObject = (JSONObject) jObj.get("capabilities");
+
+            for (Object keyStr : jAObject.keySet()) {
+                capabilities.setCapability(keyStr.toString(), jAObject.get(keyStr).toString());
+            }
+        } catch (Exception e) {
+            log.error(("No test capabilities found!"));
+        }
 
 
+        if (pCapabilities != null) {
+
+            pCapabilities = pCapabilities.replaceAll("'", "\"");
+
+            try {
+                JSONObject jsonObject = (JSONObject) new JSONParser().parse(pCapabilities);
+
+                for (Object keyStr : jsonObject.keySet()) {
+                    capabilities.setCapability(keyStr.toString(), jsonObject.get(keyStr).toString());
+                }
+            } catch (ParseException e) {
+                log.error(("No test parameter capabilities found!"));
+            }
+
+
+        }
+
+        return capabilities;
     }
-
-    return capabilities;
-  }
 
 }
