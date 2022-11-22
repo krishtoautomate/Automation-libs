4b75abf80f4da385792c6093e184d8d41ff9ea04
krish.pavulur <krish.pavulur@bell.ca>
2022-11-21 22:25:27 -0500

    listener update

--- src/test/java/com/Listeners/TestListener.java
+++ src/test/java/com/Listeners/TestListener.java
@@ -2,8 +2,8 @@ package com.Listeners;
 
 import com.DataManager.DeviceInfoReader;
 import com.ReportManager.ExtentTestManager;
-import com.ReportManager.LoggerManager;
 import com.Utilities.Constants;
+import com.Utilities.ScreenShotManager;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.MediaEntityBuilder;
@@ -11,227 +11,217 @@ import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.markuputils.MarkupHelper;
 import com.base.AppiumDriverManager;
 import com.base.Jira;
-import com.Utilities.ScreenShotManager;
-import com.base.Log;
 import io.appium.java_client.AppiumDriver;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriverException;
+import org.testng.*;
+
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Map;
 import java.util.TimeZone;
-import org.apache.log4j.Logger;
-import org.openqa.selenium.WebDriverException;
-import org.testng.IInvokedMethod;
-import org.testng.IInvokedMethodListener;
-import org.testng.ISuite;
-import org.testng.ISuiteListener;
-import org.testng.ITestListener;
-import org.testng.ITestResult;
-import org.testng.TestListenerAdapter;
 
 public class TestListener extends TestListenerAdapter implements ISuiteListener, ITestListener,
-    IInvokedMethodListener {
+        IInvokedMethodListener {
 
-  //extends TestListenerAdapter
+    //extends TestListenerAdapter
 //  protected ReportBuilder reporter = new ReportBuilder();
 
-  private static Logger log = Logger.getLogger(TestListener.class.getName());
+    private static Logger log = Logger.getLogger(TestListener.class.getName());
 
-  Jira jiraReporter = new Jira();
+    Jira jiraReporter = new Jira();
 
-  @Override
-  public void onTestStart(ITestResult testResult) {
+    @Override
+    public void onTestStart(ITestResult testResult) {
 
-    Date date = new Date();
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
-    sdf.setTimeZone(TimeZone.getTimeZone("EST"));
-    String dateANDtime = sdf.format(date.getTime());
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
+        sdf.setTimeZone(TimeZone.getTimeZone("EST"));
+        String dateANDtime = sdf.format(date.getTime());
 
-    String start = dateANDtime;
+        String start = dateANDtime;
 
-    testResult.setAttribute("start", start);
+        testResult.setAttribute("start", start);
 
-  }
-
-  @Override
-  public synchronized void onTestSuccess(ITestResult testResult) {
-    /*
-     * get device details
-     */
-    Map<String, String> testParams =
-        testResult.getTestContext().getCurrentXmlTest().getAllParameters();
-    String platForm = testParams.get("platForm");
-
-    ExtentTest test = ExtentTestManager.getTest();
-
-    // Categories
-    test.assignCategory(platForm);
-
-    System.getenv("BUILD_NUMBER");
-    System.getenv("ENVIRONMENT");
-    String testName = testResult.getMethod().getMethodName();
-
-    test.log(Status.INFO, testName + " - Completed as Success");
-
-    // DB update
-    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-    LocalDateTime now = LocalDateTime.now();
-    dtf.format(now);
-
-    // reporter.report(date_time, "LM", buildNo, environment, testName, deviceName, platForm,
-    // "PASS",
-    // "");
-
-    // Jira
-    Date date = new Date();
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
-    sdf.setTimeZone(TimeZone.getTimeZone("EST"));
-    String dateANDtime = sdf.format(date.getTime());
-
-    String testKey = null;
-    try {
-      testKey = testResult.getAttribute("testKey").toString();
-    } catch (Exception e) {
-      // ignore
     }
 
-    if (testKey != null) {
-      String start = testResult.getAttribute("start").toString();
-      String finish = dateANDtime;
-      // jiraReporter.setTestInfo("testKey", testKey);
-      // jiraReporter.setTestInfo("start", start);
-      // jiraReporter.setTestInfo("finish", finish);
-      // jiraReporter.setTestInfo("status", "PASS");
-      // jiraReporter.setTestInfo("comment", testName+"("+platForm+")");
+    @Override
+    public synchronized void onTestSuccess(ITestResult testResult) {
+        /*
+         * get device details
+         */
+        Map<String, String> testParams =
+                testResult.getTestContext().getCurrentXmlTest().getAllParameters();
+        String platForm = testParams.get("platForm");
 
-      jiraReporter.addTest(testKey, start, finish, "PASS", testName + "(" + platForm + ")");
+        ExtentTest test = ExtentTestManager.getTest();
+
+        // Categories
+        test.assignCategory(platForm);
+
+        System.getenv("BUILD_NUMBER");
+        System.getenv("ENVIRONMENT");
+        String testName = testResult.getMethod().getMethodName();
+
+        test.log(Status.INFO, testName + " - Completed as Success");
+
+        // DB update
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        dtf.format(now);
+
+        // reporter.report(date_time, "LM", buildNo, environment, testName, deviceName, platForm,
+        // "PASS",
+        // "");
+
+        // Jira
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
+        sdf.setTimeZone(TimeZone.getTimeZone("EST"));
+        String dateANDtime = sdf.format(date.getTime());
+
+        String testKey = null;
+        try {
+            testKey = testResult.getAttribute("testKey").toString();
+        } catch (Exception e) {
+            // ignore
+        }
+
+        if (testKey != null) {
+            String start = testResult.getAttribute("start").toString();
+            String finish = dateANDtime;
+            // jiraReporter.setTestInfo("testKey", testKey);
+            // jiraReporter.setTestInfo("start", start);
+            // jiraReporter.setTestInfo("finish", finish);
+            // jiraReporter.setTestInfo("status", "PASS");
+            // jiraReporter.setTestInfo("comment", testName+"("+platForm+")");
+
+            jiraReporter.addTest(testKey, start, finish, "PASS", testName + "(" + platForm + ")");
+        }
     }
-  }
 
-  @Override
-  public synchronized void onTestFailure(ITestResult testResult) {
-    /*
-     * get device details
-     */
-    Map<String, String> testParams =
-        testResult.getTestContext().getCurrentXmlTest().getAllParameters();
-    String udid = testParams.get("udid");
-    String platForm = testParams.get("platForm");
+    @Override
+    public synchronized void onTestFailure(ITestResult testResult) {
+        /*
+         * get device details
+         */
+        Map<String, String> testParams =
+                testResult.getTestContext().getCurrentXmlTest().getAllParameters();
+        String udid = testParams.get("udid");
+        String platForm = testParams.get("platForm");
 
-    System.getenv("BUILD_NUMBER");
-    System.getenv("ENVIRONMENT");
-    String testName = testResult.getMethod().getMethodName();
+        System.getenv("BUILD_NUMBER");
+        System.getenv("ENVIRONMENT");
+        String testName = testResult.getMethod().getMethodName();
 
-    Object testClass = testResult.getInstance();
-    AppiumDriver driver = AppiumDriverManager.getDriverInstance();
-//    Logger log = LoggerManager.getLogger();
-    ExtentTest test = ExtentTestManager.getTest();
-    DeviceInfoReader deviceInfoReader = new DeviceInfoReader(udid);
-    String deviceName = deviceInfoReader.getString("name");
+        AppiumDriver driver = AppiumDriverManager.getDriverInstance();
+        ExtentTest test = ExtentTestManager.getTest();
+        DeviceInfoReader deviceInfoReader = new DeviceInfoReader(udid);
+        String deviceName = deviceInfoReader.getString("name");
 
-//    if (driver != null) {
-    log.error("Test failed : " + testName + " : " + udid + "_" + deviceName);
-    try {
-      ScreenShotManager screenShotManager = new ScreenShotManager();
-      String ScreenShot = screenShotManager.getScreenshot();
+        if (driver != null) {
+            log.error("Test failed : " + testName + " : " + udid + "_" + deviceName);
+            try {
+                ScreenShotManager screenShotManager = new ScreenShotManager();
+                String ScreenShot = screenShotManager.getScreenshot();
 
-      test.fail("Failed Test case : " + testName + "\n" + testResult.getThrowable(),
-          MediaEntityBuilder.createScreenCaptureFromPath(ScreenShot).build());
+                test.fail("Failed Test case : " + testName + "\n" + testResult.getThrowable(),
+                        MediaEntityBuilder.createScreenCaptureFromPath(ScreenShot).build());
 
-      String errorXML = driver.getPageSource();
-      test.info(MarkupHelper.createCodeBlock(errorXML));
+                String errorXML = driver.getPageSource();
+                test.info(MarkupHelper.createCodeBlock(errorXML));
 
-    } catch (WebDriverException e) {
-      test.fail("Failed Test case : " + testName + "\n" + testResult.getThrowable());
-    }
+            } catch (WebDriverException e) {
+                test.fail("Failed Test case : " + testName + "\n" + testResult.getThrowable());
+            }
+        }
 
-    // Categories
-    test.assignCategory(platForm);
+        // Categories
+        test.assignCategory(platForm);
 
-    // DB
-    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-    LocalDateTime now = LocalDateTime.now();
-    dtf.format(now);
+        // DB
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        dtf.format(now);
 
-    // Emailable Test Summary
-    // reporter.report(date_time, "LM", buildNo, environment, testName, deviceName, platForm,
-    // "FAIL",
-    // testResult.getThrowable().toString());
+        // Emailable Test Summary
+        // reporter.report(date_time, "LM", buildNo, environment, testName, deviceName, platForm,
+        // "FAIL",
+        // testResult.getThrowable().toString());
 
 //    }
-    // Jira
-    Date date = new Date();
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
-    sdf.setTimeZone(TimeZone.getTimeZone("EST"));
-    String dateANDtime = sdf.format(date.getTime());
+        // Jira
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
+        sdf.setTimeZone(TimeZone.getTimeZone("EST"));
+        String dateANDtime = sdf.format(date.getTime());
 
-    String testKey = null;
-    try {
-      testKey = testResult.getAttribute("testKey").toString();
-    } catch (Exception e) {
-      // ignore
+        String testKey = null;
+        try {
+            testKey = testResult.getAttribute("testKey").toString();
+        } catch (Exception e) {
+            // ignore
+        }
+
+        if (testKey != null) {
+            String start = testResult.getAttribute("start").toString();
+            String finish = dateANDtime;
+            jiraReporter.addTest(testKey, start, finish, "FAIL", testName + "(" + platForm + ")");
+        }
     }
 
-    if (testKey != null) {
-      String start = testResult.getAttribute("start").toString();
-      String finish = dateANDtime;
-      jiraReporter.addTest(testKey, start, finish, "FAIL", testName + "(" + platForm + ")");
-    }
-  }
-
-  @Override
-  public synchronized void onTestSkipped(ITestResult testResult) {
-    /*
-     * get device details
-     */
-    Map<String, String> testParams =
-        testResult.getTestContext().getCurrentXmlTest().getAllParameters();
-    String udid = testParams.get("udid");
-    DeviceInfoReader deviceInfoReader = new DeviceInfoReader(udid);
-    String deviceName = deviceInfoReader.getString("name");
+    @Override
+    public synchronized void onTestSkipped(ITestResult testResult) {
+        /*
+         * get device details
+         */
+        Map<String, String> testParams =
+                testResult.getTestContext().getCurrentXmlTest().getAllParameters();
+        String udid = testParams.get("udid");
+        DeviceInfoReader deviceInfoReader = new DeviceInfoReader(udid);
+        String deviceName = deviceInfoReader.getString("name");
 
 //    Logger log = LoggerManager.getLogger();
-    log.warn("Test Skipped : " + testResult.getMethod().getMethodName() + " : " + udid + "_"
-        + deviceName);
+        log.warn("Test Skipped : " + testResult.getMethod().getMethodName() + " : " + udid + "_"
+                + deviceName);
 
-    ExtentReports extent = ExtentTestManager.getTest().getExtent();
+        try {
+            ExtentReports extent = ExtentTestManager.getTest().getExtent();
+            extent.removeTest(ExtentTestManager.getTest());
+        } catch (Exception e) {
+            // ignore
+        }
 
-    try {
-      extent.removeTest(ExtentTestManager.getTest());
-    } catch (Exception e) {
-      // ignore
     }
 
-
-  }
-
-  @Override
-  public void onStart(ISuite suite) {
+    @Override
+    public void onStart(ISuite suite) {
 //    reporter.initialize();// Emailable Report
 
-    Date date = new Date();
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
-    sdf.setTimeZone(TimeZone.getTimeZone("EST"));
-    String dateANDtime = sdf.format(date.getTime());
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
+        sdf.setTimeZone(TimeZone.getTimeZone("EST"));
+        String dateANDtime = sdf.format(date.getTime());
 
-    // Jira report
-    String buildNo = System.getenv("BUILD_NUMBER");
-    String jobName = System.getenv("JOB_NAME");
-    String buildUrl = System.getenv("BUILD_URL");
-    String testPlanKey = System.getenv("TEST_PLAN_KEY");
-    String excutionSummary = jobName + buildNo;
-    String excutionDescription = buildUrl;
-    String startDate = dateANDtime;
-    jiraReporter.setTestExecutionInfo("summary", excutionSummary);
-    jiraReporter.setTestExecutionInfo("testPlanKey", testPlanKey);// "MAEAUTO-311"
-    jiraReporter.setTestExecutionInfo("description", excutionDescription);
-    jiraReporter.setTestExecutionInfo("startDate", startDate);
+        // Jira report
+        String buildNo = System.getenv("BUILD_NUMBER");
+        String jobName = System.getenv("JOB_NAME");
+        String buildUrl = System.getenv("BUILD_URL");
+        String testPlanKey = System.getenv("TEST_PLAN_KEY");
+        String excutionSummary = jobName + buildNo;
+        String excutionDescription = buildUrl;
+        String startDate = dateANDtime;
+        jiraReporter.setTestExecutionInfo("summary", excutionSummary);
+        jiraReporter.setTestExecutionInfo("testPlanKey", testPlanKey);// "MAEAUTO-311"
+        jiraReporter.setTestExecutionInfo("description", excutionDescription);
+        jiraReporter.setTestExecutionInfo("startDate", startDate);
 
-  }
+    }
 
-  @Override
-  public void onFinish(ISuite suite) {
+    @Override
+    public void onFinish(ISuite suite) {
 //    String emailReport = Constants.EMAIL_REPORT;
 //    File file = new File(emailReport);
 //    if (file.delete()) {
@@ -242,28 +232,28 @@ public class TestListener extends TestListenerAdapter implements ISuiteListener,
 //      reporter.writeResults(emailReport);
 //    }
 
-    // Jira report
-    Date date = new Date();
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
-    sdf.setTimeZone(TimeZone.getTimeZone("EST"));
-    String dateANDtime = sdf.format(date.getTime());
-    String finishDate = dateANDtime;
-    jiraReporter.setTestExecutionInfo("finishDate", finishDate);
-    jiraReporter.addInfo();
-    jiraReporter.addTests();
-    jiraReporter.CreatejiraReport(Constants.JIRA_REPORT);
+        // Jira report
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
+        sdf.setTimeZone(TimeZone.getTimeZone("EST"));
+        String dateANDtime = sdf.format(date.getTime());
+        String finishDate = dateANDtime;
+        jiraReporter.setTestExecutionInfo("finishDate", finishDate);
+        jiraReporter.addInfo();
+        jiraReporter.addTests();
+        jiraReporter.CreatejiraReport(Constants.JIRA_REPORT);
 
-  }
+    }
 
-  @Override
-  public void beforeInvocation(IInvokedMethod method, ITestResult testResult) {
-    // TODO Auto-generated method stub
+    @Override
+    public void beforeInvocation(IInvokedMethod method, ITestResult testResult) {
+        // TODO Auto-generated method stub
 
-  }
+    }
 
-  @Override
-  public void afterInvocation(IInvokedMethod method, ITestResult testResult) {
-    // TODO Auto-generated method stub
-  }
+    @Override
+    public void afterInvocation(IInvokedMethod method, ITestResult testResult) {
+        // TODO Auto-generated method stub
+    }
 
 }
--- src/test/java/com/base/AppiumDriverManager.java
+++ src/test/java/com/base/AppiumDriverManager.java
@@ -1,78 +1,68 @@
 package com.base;
 
-import com.Utilities.Constants;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.TimeUnit;
-import org.testng.ITestResult;
-import org.testng.Reporter;
 
-@SuppressWarnings("rawtypes")
 public class AppiumDriverManager {
 
-  private static AppiumDriverLocalService server;
-  static Map<Long, AppiumDriver> driverMap = new ConcurrentHashMap<Long, AppiumDriver>();
-  private ThreadLocal<AppiumDriver> tlDriver = new ThreadLocal<>();
-  private CapabilitiesManager capabilitiesManager = new CapabilitiesManager();
+    static Map<Long, AppiumDriver> driverMap = new ConcurrentHashMap<Long, AppiumDriver>();
+    private static AppiumDriverLocalService server;
+    private ThreadLocal<AppiumDriver> tlDriver = new ThreadLocal<>();
+    private CapabilitiesManager capabilitiesManager = new CapabilitiesManager();
 
-  public synchronized void setDriver() throws MalformedURLException {
-
-    ITestResult iTestResult = Reporter.getCurrentTestResult();
-    Map<String, String> testParams =
-        iTestResult.getTestContext().getCurrentXmlTest().getAllParameters();
-
-    String platForm = testParams.get("platForm");
-    String REMOTE_HOST =
-        testParams.get("REMOTE_HOST") == null ? "localhost" : testParams.get("REMOTE_HOST");
-
-    if (REMOTE_HOST.equalsIgnoreCase("localhost")) {
-      AppiumService appiumService = new AppiumService();
-      server = appiumService.AppiumServer();
-      server.start();
-      REMOTE_HOST = server.getUrl().toString();
+    public static synchronized AppiumDriver getDriverInstance() {
+        return driverMap.get(Long.valueOf(Thread.currentThread().getId()));
     }
 
-    if ("Android".equalsIgnoreCase(platForm)) {
-      tlDriver.set(new AndroidDriver(new URL(REMOTE_HOST),
-          capabilitiesManager.setCapabilities()));
-    } else if ("iOS".equalsIgnoreCase(platForm)) {
-      tlDriver.set(new IOSDriver(new URL(REMOTE_HOST),
-          capabilitiesManager.setCapabilities()));
+    public static synchronized void quit() {
+        getDriverInstance().quit();
+
+        if (server.isRunning()) {
+            server.stop();
+        }
     }
 
-    driverMap.put(Thread.currentThread().getId(), tlDriver.get());
+    public synchronized void setDriver() throws MalformedURLException {
 
-    // System.out.println("Thread Id : "+ Thread.currentThread().getId());
-    // System.out.println("Session Id : "+ tlDriver.get().getSessionId());
+        ITestResult iTestResult = Reporter.getCurrentTestResult();
+        Map<String, String> testParams =
+                iTestResult.getTestContext().getCurrentXmlTest().getAllParameters();
 
-    getDriverInstance().manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        String platForm = testParams.get("platForm");
+        String REMOTE_HOST =
+                testParams.get("REMOTE_HOST") == null ? "localhost" : testParams.get("REMOTE_HOST");
 
-  }
+        if (REMOTE_HOST.equalsIgnoreCase("localhost")) {
+            AppiumService appiumService = new AppiumService();
+            server = appiumService.AppiumServer();
+            server.start();
+            REMOTE_HOST = server.getUrl().toString();
+        }
 
-  public static synchronized AppiumDriver getDriverInstance() {
-    return driverMap.get(Long.valueOf(Thread.currentThread().getId()));
-  }
+        if ("Android".equalsIgnoreCase(platForm)) {
+            tlDriver.set(new AndroidDriver(new URL(REMOTE_HOST),
+                    capabilitiesManager.setCapabilities()));
+        } else if ("iOS".equalsIgnoreCase(platForm)) {
+            tlDriver.set(new IOSDriver(new URL(REMOTE_HOST),
+                    capabilitiesManager.setCapabilities()));
+        }
 
-  public static synchronized void quit() {
-    getDriverInstance().quit();
+        driverMap.put(Thread.currentThread().getId(), tlDriver.get());
 
-    if (server.isRunning()) {
-      server.stop();
+        // System.out.println("Thread Id : "+ Thread.currentThread().getId());
+        // System.out.println("Session Id : "+ tlDriver.get().getSessionId());
+
+        getDriverInstance().manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
     }
-
-  }
-
-//  private boolean isAndroid() {
-//    ITestResult iTestResult = Reporter.getCurrentTestResult();
-//    Map<String, String> testParams =
-//        iTestResult.getTestContext().getCurrentXmlTest().getAllParameters();
-//    return "Android".equalsIgnoreCase(testParams.get("platForm"));
-//  }
 }
