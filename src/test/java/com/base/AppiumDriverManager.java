3978c879b3c4be06a6efe58cfb54004dda460708
krish.pavulur <krish.pavulur@bell.ca>
2023-08-16 12:44:40 -0400

    update gradle dependencies

--- src/test/java/com/base/AppiumDriverManager.java
+++ src/test/java/com/base/AppiumDriverManager.java
@@ -1,9 +1,11 @@
 package com.base;
 
+import com.Listeners.TestListener;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.remote.SessionId;
 import org.testng.ITestResult;
 import org.testng.Reporter;
@@ -19,6 +21,8 @@ public class AppiumDriverManager {
     private static AppiumDriverLocalService server;
     private ThreadLocal<AppiumDriver> tlDriver = new ThreadLocal<>();
     private CapabilitiesManager capabilitiesManager = new CapabilitiesManager();
+
+    private static Logger log = Logger.getLogger(TestListener.class.getName());
 
     public static synchronized AppiumDriver getDriverInstance() {
         return driverMap.get(Thread.currentThread().getId());
@@ -64,15 +68,13 @@ public class AppiumDriverManager {
                         capabilitiesManager.setCapabilities("IOS")));
             }
         } catch (Exception e) {
-//            System.out.println("LMessage : "+ e.getLocalizedMessage());
-            System.out.println("Message : " + e.getMessage());
+            log.error("Message : " + e.getMessage());
             throw new RuntimeException(e.getLocalizedMessage());
         }
 
         driverMap.put(Thread.currentThread().getId(), tlDriver.get());
 
-        // System.out.println("Thread Id : "+ Thread.currentThread().getId());
-        System.out.println("SessionId : " + tlDriver.get().getSessionId());
+        log.info("SessionId : " + tlDriver.get().getSessionId());
 
         getDriverInstance().manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
