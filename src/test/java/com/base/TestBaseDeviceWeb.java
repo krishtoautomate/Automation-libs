38279a559d0f432f1b1b77bf1d58d976abc67d27
krish.pavulur <krish.pavulur@bell.ca>
2023-08-30 22:54:00 -0400

    browser base added for devices

---
+++ src/test/java/com/base/TestBaseDeviceWeb.java
@@ -0,0 +1,149 @@
+package com.base;
+
+import com.DataManager.TestDataManager;
+import com.ReportManager.ExtentTestManager;
+import com.Utilities.Constants;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import org.apache.log4j.Logger;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+import org.testng.annotations.*;
+
+import java.lang.reflect.Method;
+import java.util.Map;
+
+/**
+ * Created by Krish on 06.06.2018.
+ */
+public class TestBaseDeviceWeb {
+
+    protected static Logger log;
+    protected AppiumDriver driver;
+    protected AppiumDriverManager tlDriverFactory = new AppiumDriverManager();
+    protected ExtentTest test;
+    protected boolean isAndroid = false;
+    protected boolean isIos = false;
+    protected boolean isFrench = false;
+
+    /**
+     * Executed once before all the tests
+     */
+    @BeforeSuite(alwaysRun = true)
+    public void setupSuit(ITestContext ctx) {
+//        String suiteName = ctx.getCurrentXmlTest().getSuite().getName();
+
+
+        log = Logger.getLogger(this.getClass().getName());
+    }
+
+
+    @BeforeMethod(alwaysRun = true)
+    @Parameters({"udid", "platForm"})
+    public synchronized void Before(@Optional String udid, @Optional String platForm,
+                                    ITestContext iTestContext, Method method) {
+
+        String methodName = method.getName();
+        String className = this.getClass().getName();
+        isAndroid = platForm.equalsIgnoreCase("Android");
+        isIos = platForm.equalsIgnoreCase("iOS");
+
+        if (udid != null)
+            GlobalMapper.setUdid(udid);
+        tlDriverFactory.setBrowserDriver();
+        driver = AppiumDriverManager.getDriverInstance();
+
+        try {
+            isFrench = driver.getCapabilities().getCapability("language").toString().equalsIgnoreCase("fr");
+        } catch (Exception e) {
+            isFrench = false;
+        }
+
+        Map<String, String> testParams = iTestContext.getCurrentXmlTest().getAllParameters();
+        String pTestData = testParams.get("p_Testdata");
+        TestDataManager testData = new TestDataManager(pTestData);
+        int index = driver instanceof AndroidDriver ? 0 : 1;
+        String testKey = testData.get(index, "testKey");
+        ITestResult result = Reporter.getCurrentTestResult();
+        result.setAttribute("testKey", testKey);
+
+        // Report Content
+        test = ExtentTestManager.startTest(methodName + "(" + platForm + ")");
+
+        log.info("Test started : " + className);
+        String _udid = driver.getCapabilities().getCapability("udid").toString();
+        String deviceName = driver.getCapabilities().getCapability("deviceName").toString();
+        String platformVersion = driver.getCapabilities().getCapability("platformVersion").toString();
+        test.assignDevice(deviceName);
+
+        String[][] data = {{"<b>TestCase : </b>", className}, {"<b>Device-Name : </b>", deviceName},
+                {"<b>UDID : </b>", _udid},
+                {"<b>Platform : </b>", platForm},
+                {"<b>OsVersion : </b>", platformVersion},
+                {"<b>Jira test-key : </b>",
+                        "<a target=\"blank\" href=" + Constants.JIRA_URL + testKey + ">" + testKey +"</a>"}
+        };
+
+
+
+        test.info(MarkupHelper.createTable(data));
+    }
+
+
+    @AfterMethod(alwaysRun = true)
+    public synchronized void After() {
+
+        if (driver != null) {
+            try {
+                if (isAndroid) {
+                    ((AndroidDriver) driver).closeApp();
+                }
+
+                if (isIos) {
+                    ((IOSDriver) driver).closeApp();
+//                            .terminateApp(driver.getCapabilities().getCapability("bundleId").toString());
+                }
+                log.info("app close");
+            } catch (Exception e) {
+                // ignore
+            }
+
+            try {
+                AppiumDriverManager.quit();
+                log.info("driver quit - done");
+            } catch (Exception e) {
+                // ignore
+            }
+
+            try {
+                ExtentTestManager.getTest().info("THE END");
+                ExtentTestManager.flush();
+            } catch (Exception e) {
+                // ignore
+            }
+//            finally {
+//                log.info(Constants.EXTENT_HTML_REPORT);
+//            }
+        }
+    }
+
+    /**
+     * Executed once after all the tests
+     */
+    @AfterSuite(alwaysRun = true)
+    public void endSuit() {
+        try {
+            ExtentTestManager.flush(); // -----close extent-report
+        } catch (Exception e) {
+            // ignore
+        }
+//        finally {
+//            log.info(Constants.EXTENT_HTML_REPORT);
+//        }
+    }
+
+}
--- src/test/java/other/testcases/MobileBrowserTest.java
+++ src/test/java/other/testcases/MobileBrowserTest.java
@@ -5,18 +5,20 @@ import com.Utilities.ScreenShotManager;
 import com.aventstack.extentreports.MediaEntityBuilder;
 import com.aventstack.extentreports.markuputils.MarkupHelper;
 import com.base.TestBase;
+import com.base.TestBaseDeviceWeb;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 
-public class MobileBrowserTest extends TestBase implements ITestBase {
+public class MobileBrowserTest extends TestBaseDeviceWeb implements ITestBase {
 
   String className = this.getClass().getSimpleName();
 
   @Test
-  @Parameters({"udid"})
-  public void Mobile_Browser_Test(@Optional String udid) {
+  public void Mobile_Browser_Test() {
+
+    String udid = driver.getCapabilities().getCapability("udid").toString();
 
     test.getModel().setName(String.format("%s - %s", className, udid));
 
